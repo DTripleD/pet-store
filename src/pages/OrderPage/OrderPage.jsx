0cde5e84a9ef71b5d3d2b467c44fe0a84ac61669
Danil Deineka <tripledpadik@gmail.com>
2024-05-06 18:15:22 +0300

    Add elements to order page

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -1,46 +1,73 @@
 import Button from "../../components/Button/Button";
+import CartItem from "../../components/CartItem/CartItem";
+import { itemsInCart } from "../../data/itemsInCart";
 import css from "./OrderPage.module.scss";
 
 const OrderPage = () => {
   return (
     <section className={css.orderSection}>
-      <div className="container">
-        <h2 className={css.orderTitle}>Оформлення замовлення</h2>
-        <form>
-          <h3 className={css.orderSubTitle}>Контактні дані</h3>
-          <ul className={css.contactInfoList}>
-            <li>
-              <label className={css.contactInfoLabel}>
-                Ім’я*
-                <input type="text" className={css.contactInfoInput} />
-              </label>
+      <div className={`container ${css.orderContainer} `}>
+        <div className={css.formWrapper}>
+          <h2 className={css.orderTitle}>Оформлення замовлення</h2>
+          <form>
+            <h3 className={css.orderSubTitle}>Контактні дані</h3>
+            <ul className={css.contactInfoList}>
+              <li>
+                <label className={css.contactInfoLabel}>
+                  Ім’я*
+                  <input type="text" className={css.contactInfoInput} />
+                </label>
+              </li>
+              <li>
+                <label className={css.contactInfoLabel}>
+                  Прізвище*
+                  <input type="text" />
+                </label>
+              </li>
+              <li>
+                <label className={css.contactInfoLabel}>
+                  Номер телефону*
+                  <input type="text" />
+                </label>
+              </li>
+              <li>
+                <label className={css.contactInfoLabel}>
+                  Електронна пошта
+                  <input type="text" />
+                </label>
+              </li>
+            </ul>
+            <label>
+              <input type="checkbox" />
+              Не телефонувати мені
+            </label>
+            <h3 className={css.orderSubTitle}>Доставка</h3>
+            <Button type="submit" text="Замовлення підтверджую" />
+          </form>
+        </div>
+        <div className={css.orderInfoWrapper}>
+          <ul>
+            {itemsInCart.map((item) => (
+              <CartItem key={item.id} item={item} />
+            ))}
+          </ul>
+          <ul className={css.priceList}>
+            <li className={css.priceItem}>
+              <p className={css.totalPrice}>До сплати</p>
+              <p className={`${css.totalPrice} ${css.totalPriceNumber}`}>
+                970 грн
+              </p>
             </li>
-            <li>
-              <label className={css.contactInfoLabel}>
-                Прізвище*
-                <input type="text" />
-              </label>
+            <li className={css.priceItem}>
+              <p className={css.partPrice}>2 товари</p>
+              <p className={css.partPrice}>900 грн</p>
             </li>
-            <li>
-              <label className={css.contactInfoLabel}>
-                Номер телефону*
-                <input type="text" />
-              </label>
-            </li>
-            <li>
-              <label className={css.contactInfoLabel}>
-                Електронна пошта
-                <input type="text" />
-              </label>
+            <li className={css.priceItem}>
+              <p className={css.partPrice}>Доставка</p>
+              <p className={css.partPrice}>70 грн</p>
             </li>
           </ul>
-          <label>
-            <input type="checkbox" />
-            Не телефонувати мені
-          </label>
-          <h3 className={css.orderSubTitle}>Доставка</h3>
-          <Button type="submit" text="Замовлення підтверджую" />
-        </form>
+        </div>
       </div>
     </section>
   );
